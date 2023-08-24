31fe85b7a44159d7159dec76d204f50aafbfdcbc
wcrd <11898748+wcrd@users.noreply.github.com>
2023-08-24 09:32:07 -0600

    - Remove base path for gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -15,9 +15,9 @@ const config = {
 			precompress: false,
 			strict: true
 		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/graph-vis-testing' : ''
-		}
+		// paths: {
+		// 	base: process.env.NODE_ENV === 'production' ? '/graph-vis-testing' : ''
+		// }
 	},
 	preprocess: vitePreprocess()
 };
