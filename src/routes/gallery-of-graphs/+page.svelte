550807b6e6ea6bde057f3429bcfce1332ce1e276
wcrd <11898748+wcrd@users.noreply.github.com>
2023-08-23 21:39:20 -0600

    - Add Directed Graph - Add Tidy Tree

---
+++ src/routes/gallery-of-graphs/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import DirectedGraph from "$lib/components/DirectedGraph.svelte";
+    import TidyTree from "$lib/components/TidyTree.svelte";
+</script>
+
+<div class="container mx-auto">
+    <h1 class="text-2xl my-2">Gallery of Graphs</h1>
+    <hr/>
+    <div class="my-2">
+        <h1 class="text-xl">Directed Graph</h1>
+        <DirectedGraph />
+    </div>
+    <hr>
+    <div class="my-2">
+        <h1 class="text-xl">Tidy Tree</h1>
+        <TidyTree />
+    </div>
+    
+
+</div>
+
