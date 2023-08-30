a8c7861fef86fca259a67690550a5694980aa21c
wcrd <11898748+wcrd@users.noreply.github.com>
2023-08-30 02:33:05 -0600

    - Build chart in SVG direct. Need to finish links :

---
+++ src/lib/components/SvelteTidyTree.svelte
@@ -0,0 +1,180 @@
+<script>
+	import * as d3 from 'd3';
+	import { onMount } from 'svelte';
+
+    import DataTidyTree from '../../working/data_TidyTree.json'
+
+	let el;
+
+    const data = DataTidyTree
+
+
+	const width = 928;
+
+	// Compute the tree height; this approach will allow the height of the
+	// SVG to scale according to the breadth (width) of the tree layout.
+	const root = d3.hierarchy(data);
+	const dx = 10;
+	const dy = width / (root.height + 1);
+
+	// Create a tree layout.
+	const tree = d3.tree().nodeSize([dx, dy]);
+
+	// Sort the tree and apply the layout.
+	root.sort((a, b) => d3.ascending(a.data.type, b.data.type));
+	tree(root);
+
+
+	// Compute the extent of the tree. Note that x and y are swapped here
+	// because in the tree layout, x is the breadth, but when displayed, the
+	// tree extends right rather than down.
+	let x0 = Infinity;
+	let x1 = -x0;
+	root.each((d) => {
+		if (d.x > x1) x1 = d.x;
+		if (d.x < x0) x0 = d.x;
+	});
+
+	// Compute the adjusted height of the tree.
+	const height = x1 - x0 + dx * 2
+
+    console.debug(root)
+
+	// onMount(() => {
+	// 	const svg = d3
+    //         .select(el)
+	// 		.append('svg')
+	// 		.attr('width', width)
+	// 		.attr('height', height)
+	// 		.attr('viewBox', [-dy / 3, x0 - dx, width, height])
+	// 		.attr('style', 'max-width: 100%; height: auto; font: 10px sans-serif;');
+
+	// 	const link = svg
+	// 		.append('g')
+	// 		.attr('fill', 'none')
+	// 		.attr('stroke', '#555')
+	// 		.attr('stroke-opacity', 0.4)
+	// 		.attr('stroke-width', 1.5)
+	// 		.selectAll()
+	// 		.data(root.links())
+	// 		.join('path')
+	// 		.attr(
+	// 			'd',
+	// 			d3
+	// 				.linkHorizontal()
+	// 				.x((d) => d.y)
+	// 				.y((d) => d.x)
+	// 		);
+        
+    //     link
+    //         .attr('stroke', d => {
+    //             switch(d.target.data.rel) {
+    //                 case 'hasPoint':
+    //                     return 'green'
+    //                 case 'hasPart':
+    //                     return 'blue'
+    //                 case 'feeds':
+    //                     return 'orange'
+    //                 default:
+    //                     return '#555'
+    //             }
+    //         })
+
+	// 	const node = svg
+	// 		.append('g')
+	// 		.attr('stroke-linejoin', 'round')
+	// 		.attr('stroke-width', 3)
+	// 		.selectAll()
+	// 		.data(root.descendants())
+	// 		.join('g')
+	// 		.attr('transform', (d) => `translate(${d.y},${d.x})`);
+
+	// 	node
+    //         .filter(d => d.data.type == 'point')
+	// 		.append('circle')
+	// 		.attr('fill', (d) => (d.children ? '#555' : '#999'))
+	// 		.attr('r', d => 4);
+        
+    //     node
+    //         .filter(d => d.data.type == 'entity')
+    //         .append('rect')
+    //         .attr('x', -4)
+    //         .attr('y', -4)
+    //         .attr('width', 8)
+    //         .attr('height', 8)
+    //         .attr('fill', d => d.data.name == 'target' ? 'red' : 'black')
+
+	// 	node
+	// 		.append('text')
+	// 		.attr('dy', '0.31em')
+	// 		.attr('x', (d) => (d.children ? -7 : 6))
+	// 		.attr('text-anchor', (d) => (d.children ? 'end' : 'start'))
+	// 		.text((d) => d.data.name)
+	// 		.clone(true)
+	// 		.lower()
+	// 		.attr('stroke', 'white');
+	// });
+
+	function pathColor(pathType){
+		switch(pathType) {
+			case 'hasPoint':
+				return 'green'
+			case 'hasPart':
+				return 'blue'
+			case 'feeds':
+				return 'orange'
+			default:
+				return '#555'
+		}
+	}
+
+	console.debug(d3, root)
+
+</script>
+
+<div class="border rounded-md my-1 py-5 flex flex-col items-center">
+    
+    <div bind:this={el}>
+		<svg 
+			{width} 
+			{height} 
+			viewBox={[-dy / 3, x0 - dx, width, height]} 
+			style='max-width: 100%; height: auto; font: 10px sans-serif;'
+		>
+			<g style="stroke-width: 3; stroke-linejoin: 'round'">
+				{#each root.descendants() as d }
+					<g transform={`translate(${d.y},${d.x})`}>
+						{#if d.data.type=="point"}
+						<circle fill="#555" r=4></circle>
+						{:else if d.data.type == 'entity'}
+						<rect x=-4 y=-4 width=8 height=8 fill={d.data.name=='target' ? 'red' : 'black'}></rect>
+						{/if}
+
+						<text dy="0.31em" x={d.children ? -7 : 6} text-anchor={d.children ? 'end' : 'start'} stroke="white">{d.data.name}</text>
+						<text dy="0.31em" x={d.children ? -7 : 6} text-anchor={d.children ? 'end' : 'start'}>{d.data.name}</text>
+					</g>
+				{/each}
+			</g>
+			<g fill='none' stroke='#555' stroke-opacity=0.4 stroke-width=1.5>
+				{#each root.links() as l }
+				<path d={(d3.linkHorizontal().x((l) => l.y).y((l) => l.x))} stroke={pathColor(l.target.data.rel)}></path>
+				{/each}
+			</g>
+		</svg>
+	</div>
+
+    <div id="legend" class="flex flex-row gap-x-5 mt-5">
+        <div class="flex flex-row gap-x-2 items-center">
+            <span class="block h-3 w-3 rounded-full bg-yellow-500"></span>
+            Has Part
+        </div>
+        <div class="flex flex-row gap-x-2 items-center">
+            <span class="block h-3 w-3 rounded-full bg-blue-500"></span>
+            Feeds
+        </div>
+        <div class="flex flex-row gap-x-2 items-center">
+            <span class="block h-3 w-3 rounded-full bg-green-500"></span>
+            Has Point
+        </div>
+    </div>
+</div>
---
+++ src/routes/sveltify-d3/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import TidyTree from "$lib/components/TidyTree.svelte";
+    import SvelteTidyTree from "$lib/components/SvelteTidyTree.svelte";
+</script>
+
+<div class="container mx-auto">
+    <h1 class="text-2xl my-2">Gallery of Graphs</h1>
+    <hr/>
+    <div class="my-2">
+        <h1 class="text-xl">Tidy Tree</h1>
+        <TidyTree />
+    </div>
+    <hr>
+    <div class="my-2">
+        <h1 class="text-xl">Svelte Tidy Tree</h1>
+        <SvelteTidyTree />
+    </div>
+
+</div>
+
