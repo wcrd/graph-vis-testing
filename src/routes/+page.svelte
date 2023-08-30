d23a8073f62c97406c5fd1d7e966f5580dffdc27
wcrd <11898748+wcrd@users.noreply.github.com>
2023-08-30 17:27:42 -0600

    - Add links to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-xl">Welcome to Graph Vis Testing</h1>
+<hr>
+<div>
+    <div class="mt-2">
+        <a class="text-blue-500" href="/gallery-of-graphs">Gallery Of Graphs</a>
+        <p class="ml-5">Comparison of different d3 graphs for visualising logic matches</p>
+    </div>
+    <div class="mt-2">
+        <a class="text-blue-500" href="/sveltify-d3">D3 vs Sveltified-D3</a>
+        <p class="ml-5">Comparison of the same graph made two different ways</p>
+    </div>
+    
+</div>
